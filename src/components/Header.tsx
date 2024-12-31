976b513c6e675fc0ed51f45527e6cdbc32950cb3
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-12-31 00:49:35 -0600

    Add page navigation with React Router

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,27 @@
+import { NavLink } from "react-router-dom"
 
 function Header() {
+
   return (
     <header className="bg-slate-800">
-      <div className="mx-auto container px-5 py-16">
+      <div className="mx-auto container p-5">
         <div className="flex justify-between items-center">
           <div>
-            <img className="w-32" src="/logo.svg" alt="logotype" />
+            <img className="w-24" src="/logo.svg" alt="logotype" />
           </div>
-          <nav>
 
+          <nav className="flex gap-4">
+            <NavLink 
+              to="/"
+              className={({isActive}) =>
+                 isActive ? 'text-orange-400 uppercase font-bold' : 'text-white uppercase font-bold'
+            }>Home</NavLink>
+
+            <NavLink 
+              to="/favorites"
+              className={({isActive}) =>
+                isActive ? 'text-orange-400 uppercase font-bold' : 'text-white uppercase font-bold'
+           }>Favorites</NavLink>
           </nav>
         </div>
       </div>
